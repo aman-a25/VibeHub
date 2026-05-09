7b586ee301fb990e9fc6c7037dfd72723fe1f8ed
Aman kumar <ak9910450375@gmail.com>
2026-05-09 16:51:59 +0530

    we have added server configuration so that all the queries run by hibernate will be shown and hibernate will have DDL access

--- src/main/java/com/myorg/vibehub/repository/UserRepository.java
+++ src/main/java/com/myorg/vibehub/repository/UserRepository.java
@@ -6,4 +6,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    // We do not have to define methods for most of the CRUD operation
+    // Because those methods are given to us by JPA repository Those are predefined methods by JPA Repository
+
+    // But there are some situations when we actually need to create our own methods
+    // In those situation we use custom Finder methods
+
+    //Custom Finder methods
+
+    //Here the reason why we are only receiving a single entity of user
+    // is because in entity model of User we have defined that username is also a unique entity
+    // Otherwise we can receive list of user
+    User findByuserName (String username);
+
 }
